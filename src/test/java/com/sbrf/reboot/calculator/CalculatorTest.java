2066edde21af051a7a41b2b10af8b41b753344ab
broplz <zadr13@gmail.com>
2021-12-24 01:21:00 +0300

    added Calculator class with 7 methods, with tests(1 test for each method)

--- src/test/java/com/sbrf/reboot/calculator/CalculatorTest.java
+++ src/test/java/com/sbrf/reboot/calculator/CalculatorTest.java
@@ -1,6 +1,9 @@
 package com.sbrf.reboot.calculator;
 
+import com.sbrf.reboot.Calculator;
 import org.junit.jupiter.api.Test;
+
+import java.math.BigInteger;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -26,6 +29,20 @@ class CalculatorTest {
         assertEquals(3, new Calculator().getDivision(9, 3));
     }
 
+    @Test
+    void getFactorial() {
+        assertEquals(BigInteger.valueOf(3_628_800), new Calculator().getFactorial(10));
+    }
+
+    @Test
+    void getFactorialSub() {
+        assertEquals(BigInteger.valueOf(357840), new Calculator().getFactorialSub(9, 7));
+    }
+
+    @Test
+    void getFactorialAdd() {
+        assertEquals(BigInteger.valueOf(725760), new Calculator().getFactorialAdd(9, 9));
+    }
     @Test
     void classHasSevenMethods() {
         assertEquals(7, Calculator.class.getMethods().length - Object.class.getMethods().length);
